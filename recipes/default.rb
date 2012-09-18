fe07fc92a19c9c7249193d8f72f1628224b62ac1
Mark Bate <mark.bate@mac.com>
2012-09-18 13:39:01 +0200

    fixed the merging / reverting issue

--- recipes/default.rb
+++ recipes/default.rb
@@ -37,7 +37,7 @@ directory "/etc/ganglia"
 
 case node[:ganglia][:unicast]
 when true
-  host = search(:node, "role:#{node['ganglia']['server_role']} AND chef_environment:#{node.chef_environment}").map {|node| node.ipaddress}.first
+  host = search(:node, "role:#{node['ganglia']['server_role']} AND chef_environment:#{node.chef_environment}").map {|node| node.ipaddress}
   Chef::Log.info("the node's ipaddress is: #{node.ipaddress}")
   Chef::Log.info("the host is: #{host}")
   if host.empty? 
