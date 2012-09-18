af9fa63a957070356ee7cec232bbca5f74706974
Mark Bate <mark.bate@mac.com>
2012-09-18 13:40:16 +0200

    Revert "fixed the merging / reverting issue"
    This reverts commit fe07fc92a19c9c7249193d8f72f1628224b62ac1.

--- recipes/default.rb
+++ recipes/default.rb
@@ -37,7 +37,7 @@ directory "/etc/ganglia"
 
 case node[:ganglia][:unicast]
 when true
-  host = search(:node, "role:#{node['ganglia']['server_role']} AND chef_environment:#{node.chef_environment}").map {|node| node.ipaddress}
+  host = search(:node, "role:#{node['ganglia']['server_role']} AND chef_environment:#{node.chef_environment}").map {|node| node.ipaddress}.first
   Chef::Log.info("the node's ipaddress is: #{node.ipaddress}")
   Chef::Log.info("the host is: #{host}")
   if host.empty? 
